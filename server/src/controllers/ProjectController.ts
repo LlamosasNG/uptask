9573bc54d3f76c2351deef0a50c1bdf7bc0a5626
LlamosasNG <gonzalezllamosas07@gmail.com>
2025-01-28 13:53:31 -0600

    Union del backend y frontend Se realizan las primeras pruebas con el endpoint de crear proyectos.

---
+++ server/src/controllers/ProjectController.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from "express";
+import Project from "../models/Project";
+
+export class ProjectController {
+  static createProjects = async (req: Request, res: Response) => {
+    const project = new Project(req.body);
+    try {
+      await project.save();
+      res.send("Proyecto creado correctamente");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  static getAllProjects = async (req: Request, res: Response) => {
+    try {
+      const projects = await Project.find();
+      res.json(projects);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  static getProjectById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+      const project = await Project.findById(id).populate("tasks");
+      if (!project) {
+        const error = new Error("Proyecto no encontrado");
+        res.status(404).json({ error: error });
+        return;
+      }
+      res.json(project);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  static updateProject = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+      const project = await Project.findById(id);
+      if (!project) {
+        const error = new Error("Proyecto no encontrado");
+        res.status(404).json({ error: error });
+        return;
+      }
+
+      project.projectName = req.body.projectName;
+      project.clientName = req.body.clientName;
+      project.description = req.body.description;
+      await project.save();
+      
+      res.send("Proyecto actualizado");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  static deleteProject = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+      const project = await Project.findById(id);
+      if (!project) {
+        const error = new Error("Proyecto no encontrado");
+        res.status(404).json({ error: error });
+        return;
+      }
+      await project.deleteOne();
+      res.send("Proyecto eliminado");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors"; 
+import { corsConfig } from "./config/cors";
+import { connectDB } from "./config/db";
+import projectRoutes from "../src/routes/projectRoutes";
+
+dotenv.config();
+connectDB();
+
+// Create Express server
+const app = express();
+app.use(cors(corsConfig));
+
+app.use(express.json());
+
+app.use("/api/projects", projectRoutes);
+
+export default app;
