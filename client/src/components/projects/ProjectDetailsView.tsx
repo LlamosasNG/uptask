021c0f6ca12a7741a6d830a6d38f31687a00393f
LlamosasNG <gonzalezllamosas07@gmail.com>
2025-01-30 21:46:23 -0600

    Cambios en el backend Implementación de la primera parte del formulario.

--- client/src/components/projects/ProjectDetailsView.tsx
+++ client/src/components/projects/ProjectDetailsView.tsx
@@ -1,5 +1,40 @@
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getProjectById } from "@/api/ProjectAPI";
+import AddTaskModal from "../tasks/AddTaskModal";
+
 export default function ProjectDetailsView() {
-  return (
-    <div>ProjectDetailsView</div>
-  )
+  const navigate = useNavigate();
+  const params = useParams();
+  const projectId = params.projectId!;
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["editProject", projectId],
+    queryFn: () => getProjectById(projectId),
+  });
+
+  if (isLoading) return "Cargando...";
+  if (isError) return <Navigate to={"/404"} />;
+
+  if (data)
+    return (
+      <>
+        <h1 className="text-5xl font-black">{data.projectName}</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">
+          {data.description}
+        </p>
+
+        <nav>
+          <button
+            type="button"
+            className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors mt-3"
+            onClick={() => navigate(location.pathname + "?newTask=true")}
+          >
+            Agregar tarea
+          </button>
+        </nav>
+        
+        <AddTaskModal />
+      </>
+    );
 }
--- client/src/views/projects/EditProjectView.tsx
+++ client/src/views/projects/EditProjectView.tsx
@@ -13,8 +13,6 @@ export default function EditProjectView() {
   });
 
   if (isLoading) return "Cargando...";
-  if (isError) return <Navigate to={"/404"} />;
-  console.log(data);
-
+  if (isError) return <Navigate to={"/404"} />
   if(data) return <EditProjectForm data={data} projectId={projectId}/>
 } 
